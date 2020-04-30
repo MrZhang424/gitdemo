415614113a0cad8aecf2a91360f2fbf7bfd245b5
roy88888888 <1156000205@qq.com>
2020-04-30 10:38:13 +0800

    roy edit

--- src/main/java/com/roy/App.java
+++ src/main/java/com/roy/App.java
@@ -1,5 +1,7 @@
 package com.roy;
 
+import com.roy.service.TestService;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,8 @@ public class App
 {
     public static void main( String[] args )
     {
+        TestService ts =new TestService();
         System.out.println( "这是第一个和GitHub有关的项目" );
+        System.out.println(ts.getMe());
     }
 }
---
+++ src/main/java/com/roy/service/TestService.java
@@ -0,0 +1,7 @@
+package com.roy.service;
+
+public class TestService {
+    public String getMe(){
+        return "my name is roy";
+    }
+}
