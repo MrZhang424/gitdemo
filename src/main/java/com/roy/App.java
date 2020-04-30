f7621e33b7359e14adc2b6d8c2aca26339ebe9ff
roy88888888 <1156000205@qq.com>
2020-04-30 10:35:26 +0800

    roy edit

---
+++ src/main/java/com/roy/App.java
@@ -0,0 +1,13 @@
+package com.roy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "这是第一个和GitHub有关的项目" );
+    }
+}
